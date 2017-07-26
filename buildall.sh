00a069545ad890b2dbfba963885ab28898dc7a49
mikevictim <mikevictim@gmail.com>
2017-07-26 20:33:41 +0300

    updated compose file + many minor changes

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in hadoop hadoop-namenode hadoop-datanode spark; do
+for i in hadoop hadoop-namenode hadoop-datanode spark hadoop-historyserver; do
     echo Building $i
     ( cd $i && ./build.sh)
 done
